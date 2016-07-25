96a6737293a702ae99741281300f67f7e64a3fc8
mcolmene <mcolmene@asu.edu>
2016-07-24 19:37:31 -0700

    fixed some ui on pricing

--- js/index.js
+++ js/index.js
@@ -21,9 +21,38 @@ $(document).ready(function(){
 		$(this).find("span").toggleClass("faq-glyphicon");
 		$(this).find("a div").toggleClass("open");
 	});
+	
+	/*Pricing javascript*/
 	//Cached variables for later use
-	var serviceTotalSpan = $("#service-total"); //service total
+	var $serviceTotalSpan = $("#service-total"); //service total
+	var $occurence = $(".occurence"); // how often buttons
+	var $addons = $(".add-div");
 
+	//changes css and adds the appropriate amont to the total for occurrence of service
+	$occurence.click(function(){
+		var $this = $(this);
+		$(".occurence.selected").toggleClass("selected").find("span").toggleClass("selected-text");
+		$this.toggleClass("selected").find("span").toggleClass("selected-text");
+		switch ($this.find("span").text()) {
+			case "One Time": {
+				
+				break;
+			}
+			case "Weekly" : {
+				break;
+			}
+			case "Bi-weekly" : {
+				break;
+			}
+			case "Monthly" : {
+				break;
+			}
+		}
+	});
+	$addons.click(function() {
+		var $this = $(this);
+		$this.toggleClass("selected").find("span").toggleClass("selected-text");
+	});
 	var bedroomFlag, bathroomFlag, sizeFlag = false;
 	var prevBedTotal = 0;
 	$(".home-details").change(function(){
@@ -31,7 +60,7 @@ $(document).ready(function(){
 
 		switch(select.attr("id")) {
 			case "bedroom-select": {
-				var serviceTotal = parseInt(serviceTotalSpan.text())
+				var serviceTotal = parseInt($serviceTotalSpan.text())
 				if(bedroomFlag) {
 					serviceTotal -= prevBedCost;
 				}
@@ -41,7 +70,7 @@ $(document).ready(function(){
 				prevBedCost = bedroomCost;
 				var total = serviceTotal + bedroomCost;
 				var formattedOutput = total.toFixed(2)
-				serviceTotalSpan.text(formattedOutput);
+				$serviceTotalSpan.text(formattedOutput);
 				bedroomFlag = true;
 				break;
 			}
