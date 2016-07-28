c03e176d6ab5b8551cb212e0467967ecf88d6339
mcolmene <mcolmene@asu.edu>
2016-07-27 21:21:18 -0700

    changed to carousel and images

--- js/coupons.js
+++ js/coupons.js
@@ -5,7 +5,7 @@ var src = document.getElementById('couponsTemplate').textContent;
     	"coupons" : [
     		{
     			"couponValue" : "10%",
-    			"coupon" : "Off your first cleaning",
+    			"coupon" : "Off your first cleaning service",
     			"description" : "Set up your first cleaning with us and you will get 10% off your total cleaning services!"
     		},
     		{
@@ -14,7 +14,7 @@ var src = document.getElementById('couponsTemplate').textContent;
     			"description" : "Refer a friend or a family member to our services and we will take off 20 dollars off your next cleaning!"
     		}
     	],
-    	"desclaimer" : "*coupons are subject to change and deemed invalid under certain circumstances. Please consult with our office."
+    	"desclaimer" : "*coupons are subject to change and deemed invalid under certain circumstances. Please consult with our office. Must present this coupon before service begins."
     }
     dust.renderSource(src, data, function(err, out) { 
         $("#couponsDiv").append(out);
