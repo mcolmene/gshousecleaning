535c826e0b7fdd4606c2d741d7d018bd2e342c91
mcolmene <mcolmene@asu.edu>
2016-10-05 03:31:19 -0700

    Changed a lot
    FAQS - DONE
    How it works - DONE
    pricing - DONE
    Contact us - DONE
    still need home

--- js/faqs.js
+++ js/faqs.js
@@ -7,79 +7,103 @@
                "header1" : "Booking",
                "header2" : "Pricing",
                "header3" : "The day of cleaning",
-               "header4" : "Additional Services"
+               "header4" : "Additional Services",
+               "header5" : "Other"
            }
        ],
        "booking" : [
            {
                "faq" : " How does booking work?",
-               "answer": "we are going to put something here 1",
+               "answer": "You have the option of using our online price estimate to get an idea of what the cost of the service will be. Then you can call us at (602) 332-3259 where you can schedule an appointment to evaluate your house. After the evaluation we will give you a confirmed price on service and discuss any further details at the time of the meeting.",
                "id" : "1"
            },
            {
                "faq" : " How can I know which days are available to get my house cleaned?",
-               "answer": "we are going to put something here 2",
+               "answer": "Our services are offered through our normal business hours Monday through Friday. To find specific availability please call our number at (602) 332-3259.",
                 "id" : "2"
-           },
-           {
-               "faq" : " hello we are number 3",
-               "answer": "we are going to put something here 3",
-                "id" : "3"
            }
        ],
         "pricing" : [
            {
                "faq" : " How do you calculate the price of the service?",
-               "answer": "we are going to put something here 1",
+               "answer": "We have a flat rate based on the number of rooms of the home. If it is the first time that you will be getting your house cleaned by us we will assess if any additional charges are needed. For more information please call (602) 332-3259.",
                 "id" : "1"
            },
            {
                "faq" : " Do you have a cancelation policy?",
-               "answer": "we are going to put something here 2",
+               "answer": "Yes. Our cancelation policy is we require at least a 24 hour notice of an cancelation. If it is within the 24 hour window there will be a cancelation fee of 20% of the total cost of the day's service.",
                 "id" : "2"
            },
            {
                "faq" : " What method of payment do you accept?",
-               "answer": "we are going to put something here 3",
+               "answer": "We accept cash payments, checks and major credit cards. Credit cards will be charged an additional 2.75%.",
                 "id" : "3"
            },
             {
                "faq" : " How do I use a coupon or a referral discount?",
-               "answer": "we are going to put something here 3",
+               "answer": "When the evaluation is being done on your house you can show the coupon where we will apply it before you pay. No need to print.",
                 "id" : "4"
            }
        ],
         "cleaning" : [
             {
                "faq" : " How many cleaners should arrive for my appointment?",
-               "answer": "we are going to put something here 1",
+               "answer": "There will be two-three cleaners who will come to your home and ensure that a quality clean is done to your home. You will get to know who your cleaners are at the time you schedule your appointment. We certify that our cleaners are trustworthy and trained to clean your home.",
                 "id" : "1"
            },
            {
                "faq" : " How long will my cleaning appointment take?",
-               "answer": "we are going to put something here 2",
+               "answer": "We will give you an estimate at the time we evaluate your home. Average time for new homes receiving our service for the first time is three hours to ensure quality. After, recurring service averages from one to two hours depending on the size of the home.",
                 "id" : "2"
            },
            {
                "faq" : " Do I need to be home during the cleaning?",
-               "answer": "we are going to put something here 3",
+               "answer": "You do not need to be home at the time of the service. We do require payment before the service starts. In the case that you will not be home you can leave the payment in designated area that you discuss with the cleaners.",
                 "id" : "3"
            },
            {
                "faq" : " What will the cleaners not clean in a standard service?",
-               "answer": "we are going to put something here 3",
+               "answer": "See all the services that we provide by clicking 'Our Services' below, the home page or by click on the '?' next to 'Estimate your cleaning.' Anything else that you would like to be clean can be considred, but will incur extra charges.",
+                "id" : "4"
+           },
+           {
+               "faq" : " Do I tip the house cleaners?",
+               "answer": "There is no need to tip the cleaners since they are paid employees of G & S House Cleaning ",
                 "id" : "4"
            }
         ],
         "additional" : [
             {
                "faq" : " What are considered additional services?",
-               "answer": "we are going to put something here 1",
+               "answer": "Additional services include cleaning the inside of the fridge, base boards and inside oven. The prices can vary.",
                 "id" : "1"
            },
            {
-               "faq" : " How can I know which days are available to get my house cleaned?",
-               "answer": "we are going to put something here 2",
+               "faq" : " Are there reminders about my service?",
+               "answer": "We extend the courtesy to our customers of giving them a reminder about their scheduled service 24 hours in advanced. If you would like to opt out of this service please let one of representatives know to take you off our list.",
+                "id" : "2"
+           }
+        ],
+        "other" : [
+            {
+               "faq" : " Air conditioning, electricity, hot water and Lock-outs",
+               "answer": "All appointments must allow the cleaners to have access to the property during the appointment time.  Customers must also ensure there is electricity and hot water in the home. In addition, the air conditioning in the home must be functioning between the months of April and November. If any of the these conditions are not met by the customer, the cleaners may choose to not complete the service and Queen of Maids MAY COLLECT AND CHARGE THE FULL PRICE OF THE APPOINTMENT. If the customer reschedules their appointment and remedies the condition, 50% of the collected amount may go towards the cost of the newly scheduled appointment.",
+                "id" : "1"
+           },
+           {
+               "faq" : " Lifting of heavy items or hard to reach areas",
+               "answer": "Our cleaners will not move anything heavier than 30lbs. If you would like the cleaners to clean behind large items such as refridgerators, ovens or large furniture, please move those items prior to your appointment.",
+                "id" : "2"
+           },
+           {
+               "faq" : " Pet waste clean up",
+               "answer": "An additional fee will be collected if a cleaner is asked to clean or remove pet waste or urine. The charge will be a flat rate depending on the extent of the incident.",
+                "id" : "2"
+           },
+
+           {
+               "faq" : " Loss or broken items policy",
+               "answer": "If loss or damage to your property occurs, notification to G & S House cleaning should be made within 48 hours of the incident. You can communicate via email or phone. Once you have notified us we will send you a form to fill out and the form needs to be returned to us within 10 days of the incident.",
                 "id" : "2"
            }
         ]
--- js/index.js
+++ js/index.js
@@ -13,77 +13,248 @@ $(document).ready(function(){
     		$(".listItems").attr("data-target", "");
 	    }
 	});
-	$(".faq-divs").click(function() {
-		$(this).find(".toggle-box").slideToggle("fast");
-		$(this).find("span").toggleClass("glyphicon-minus-sign");
-		$(this).find("span").toggleClass("glyphicon-plus-sign");
-		$(this).find("span").toggleClass("faq-glyphicon-open");
-		$(this).find("span").toggleClass("faq-glyphicon");
-		$(this).find("a div").toggleClass("open");
-	});
-	
-	/*Pricing javascript*/
-	//Cached variables for later use
-	var $serviceTotalSpan = $("#service-total"); //service total
-	var $occurence = $(".occurence"); // how often buttons
-	var $addons = $(".add-div");
+	$(function () {
+  		$('[data-toggle="tooltip"]').tooltip()
+	})
+	//MODALS
+	var $careerModal = $("#careerModal");
+	var $servicesModal = $("#servicesModal")
+	//---------------- HOME Section ----------------//
 
-	//changes css and adds the appropriate amont to the total for occurrence of service
-	$occurence.click(function(){
-		var $this = $(this);
-		$(".occurence.selected").toggleClass("selected").find("span").toggleClass("selected-text");
-		$this.toggleClass("selected").find("span").toggleClass("selected-text");
-		switch ($this.find("span").text()) {
-			case "One Time": {
-				
-				break;
-			}
-			case "Weekly" : {
-				break;
-			}
-			case "Bi-weekly" : {
-				break;
-			}
-			case "Monthly" : {
-				break;
-			}
-		}
-	});
-	$addons.click(function() {
-		var $this = $(this);
-		$this.toggleClass("selected").find("span").toggleClass("selected-text");
-	});
-   console.log($("#howItWorks").length);
-   $('#howItWorks').click(function(){
-   		console.log("hello")
-   })
-	var bedroomFlag, bathroomFlag, sizeFlag = false;
-	var prevBedTotal = 0;
-	$(".home-details").change(function(){
-		var select = $(this);
+	//---------------- HOW IT WORKS functions ----------------//
 
-		switch(select.attr("id")) {
-			case "bedroom-select": {
-				var serviceTotal = parseInt($serviceTotalSpan.text())
-				if(bedroomFlag) {
-					serviceTotal -= prevBedCost;
+	//---------------- PRICING functions ----------------//
+		//Pricing cached variables 
+		var $serviceTotalSpan = $("#service-total"); //service total
+		var $occurence = $(".occurence"); // how often buttons
+		var $addons = $(".add-div");
+		var $homeDetails = $(".home-details");
+		var $infoServices = $("#infoServices");
+
+		//PRICING bindings
+		$occurence.click(occurenceClick);
+		$addons.click(addonClick);
+		$homeDetails.change(homeDetailsChange);
+
+		$infoServices.click(showServicesModal);
+				//PRICING functions
+
+		//changes css and adds the appropriate amont to the total for occurrence of service
+		var occurrenceMultiplier = 1;
+		var oneTimeFlag = false;
+		function occurenceClick() {
+			var $this = $(this);
+			$(".occurence.selected").toggleClass("selected").find("span").toggleClass("selected-text");
+			$this.toggleClass("selected").find("span").toggleClass("selected-text");
+			var serviceTotal = parseFloat($serviceTotalSpan.text()); //grabs current total of estimate
+			switch ($this.find("span").text()) {
+				case "One Time": {
+					costCalculate((serviceTotal * 2), 0);
+					oneTimeFlag = true;
+					break;
 				}
-				var bedroom = parseInt(select.val().split(" ")[0]);
-				var bedroomCost = bedroom *10.50;
-				console.log(bedroomCost);
-				prevBedCost = bedroomCost;
-				var total = serviceTotal + bedroomCost;
-				var formattedOutput = total.toFixed(2)
-				$serviceTotalSpan.text(formattedOutput);
-				bedroomFlag = true;
-				break;
+				case "Weekly" : {
+					occurrenceMultiplier = 1;
+					recalculateOccurence(serviceTotal);
+					break;
+				}
+				case "Bi-weekly" : {
+					occurrenceMultiplier = 1;
+					recalculateOccurence(serviceTotal);
+					break;
+				}
+				case "Monthly" : {
+					occurrenceMultiplier = 1;
+					recalculateOccurence(serviceTotal);
+					break;
+				}
 			}
-			case "bathroom-select": {
-				break;
-			}
-			case "size-select": {
-				break;
+		};
+		function recalculateOccurence(serviceTotal) {
+			if(oneTimeFlag) {
+				oneTimeFlag = false;
+				costCalculate((serviceTotal / 2), 0)
 			}
 		}
-	});	
+
+		var baseBoardFlag = false;
+		var fridgeFlag = false;
+		var ovenFlag = false;
+		var baseBoardsPrice = 25;
+		var fridgePrice = 20;
+		var ovenPrice = 15;
+		function addonClick() {
+			var $this = $(this);
+			$this.toggleClass("selected").find("span").toggleClass("selected-text");
+			var serviceTotal = parseFloat($serviceTotalSpan.text()); //grabs current total of estimate
+			if(oneTimeFlag) {
+				serviceTotal = serviceTotal/2;
+			}
+			switch ($this.attr("addon")) {
+				case "baseBoards" : {
+					if(baseBoardFlag) {
+						baseBoardFlag = false;
+						serviceTotal -= baseBoardsPrice;
+						costCalculate(serviceTotal, 0);
+					}
+					else {
+						costCalculate(serviceTotal, baseBoardsPrice);
+						baseBoardFlag = true;
+					}
+					break;
+				}
+				case "insideFridge" : {
+					if(fridgeFlag) {
+						fridgeFlag = false;
+						serviceTotal -= fridgePrice;
+						costCalculate(serviceTotal, 0);
+					}
+					else {
+						costCalculate(serviceTotal, fridgePrice)
+						fridgeFlag = true;
+					}
+					break;
+				}
+				case "insideOven" : {
+					if(ovenFlag) {
+						ovenFlag = false;
+						serviceTotal -= ovenPrice;
+						costCalculate(serviceTotal, 0);
+					}
+					else {
+						costCalculate(serviceTotal, ovenPrice)
+						ovenFlag = true;
+						
+					}
+					break;
+				}
+			}
+		};
+	   
+		var bedroomFlag, bathroomFlag, sizeFlag = false;
+		var prevBedCost = 0;
+		var prevBathCost = 0;
+		var prevSizeCost = 0;
+		var costOfBedroom = 14.50;
+		var costOfBathroom = 10.50;
+		function homeDetailsChange() {
+			var select = $(this);
+			var serviceTotal = parseFloat($serviceTotalSpan.text()); //grabs current total of estimate
+			if(oneTimeFlag) {
+				serviceTotal = serviceTotal/2;
+			}
+			switch(select.attr("id")) {
+				case "bedroom-select": {
+					if(bedroomFlag) {
+						serviceTotal -= prevBedCost;
+					}
+					var bedroom = parseInt(select.val().split(" ")[0]);
+					var bedroomCost = bedroom * costOfBedroom;
+					console.log(bedroomCost);
+					prevBedCost = bedroomCost;
+					
+					costCalculate(serviceTotal, bedroomCost);
+					bedroomFlag = true;
+					break;
+				}
+				case "bathroom-select": {
+					if(bathroomFlag) {
+						serviceTotal -= prevBathCost;
+					}
+					var bathroom = parseInt(select.val().split(" ")[0]);
+					var bathroomCost = bathroom * costOfBathroom;
+					console.log(bathroomCost);
+					prevBathCost = bathroomCost;
+					costCalculate(serviceTotal, bathroomCost);
+					bathroomFlag = true;
+					break;
+				}
+			}
+		};	
+		function costCalculate(serviceTotal, itemCost) {
+			var total = serviceTotal + itemCost;
+			if(oneTimeFlag) {
+				total = total * 2;
+			}
+			var formattedOutput = total.toFixed(2)
+			$serviceTotalSpan.text(formattedOutput);
+		}
+		function reset() {
+			bathroomFlag = false;
+			bedroomFlag = false;
+			sizeFlag = false;
+			baseBoardFlag = false;
+			fridgeFlag = false;
+			ovenFlag = false;
+			//reset all the values on the estimate
+		}
+
+	//---------------- CONTACT US functions ----------------//
+
+
+	//---------------- FAQS Section ----------------//
+		//FAQ cached variables
+		var $faqDivs = $(".faq-divs");
+
+		//FAQ bindings
+		$faqDivs.click(faqClick);
+
+		//FAQ functions
+		function faqClick() {
+			$(this).find(".toggle-box").slideToggle("fast");
+			$(this).find("span").toggleClass("glyphicon-minus-sign");
+			$(this).find("span").toggleClass("glyphicon-plus-sign");
+			$(this).find("span").toggleClass("faq-glyphicon-open");
+			$(this).find("span").toggleClass("faq-glyphicon");
+			$(this).find("a div").toggleClass("open");
+		};
+	//---------------- FOOTER functions ----------------//
+
+		//footer links
+		var $serviceLink = $(".services");
+		var $howItWorksLink = $("#howItWorksLink");
+		var $couponsLink = $("#coupons");
+		var $careerLink = $("#careers");
+		var $termsLink = $("#terms");
+		var $privacyLink = $("#privacy");
+		var $faqLink = $("#faqsLink");
+		var $contactLink = $("#contactLink");
+
+		//footer bindings
+		$howItWorksLink.click(showTab);
+		$contactLink.click(showTab);
+		$faqLink.click(showTab);
+
+		$careerLink.click(showCareerModal);
+		$serviceLink.click(showServicesModal)
+
+		//footer functions
+		function showTab() {
+			var tab = $(this).attr("id");
+			switch(tab) {
+				case "howItWorksLink" : {
+					tab = "howItWorks";
+					break;
+				}
+				case "faqsLink" : {
+					tab = "faqs";
+					break;
+				}
+				case "contactLink" : {
+					tab = "contactUs";
+					break;
+				}
+			}
+			$(".nav-pills a[href='#"+ tab +"']").tab('show');
+			$("body").scrollTop(0);
+			//$("body").animate({ scrollTop: 0 }, 600);
+		}
+		function showCareerModal() {
+			$careerModal.modal("show");
+		}
+		function showServicesModal() {
+			$servicesModal.modal('show');
+		}
+	
+	
 });
